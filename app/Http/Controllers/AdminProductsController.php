b52d69094615b45ef34c3e4d229bbb336a4178d6
Eduardo Couto <eduardo.lcouto@gmail.com>
2016-01-11 22:03:42 -0200

    Criado models Product e Category Criado as rotas para a listagem dos models criados Criado os controller AdminProductsController e AdminCategoriesController

---
+++ app/Http/Controllers/AdminProductsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Product;
+use Illuminate\Http\Request;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+
+class AdminProductsController extends Controller
+{
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function index()
+    {
+        $products = $this->product->all();
+
+        return view('products',compact('products'));
+    }
+}
